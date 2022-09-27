f5920f99aeef2386a54881211bfcf5e239b5cb2b
BarbaraNyan <var7773@gmail.com>
2022-09-27 12:15:35 +0300

    Add error fragment

---
+++ app/src/main/java/com/example/trainee_test/fragments/ErrorFragment.kt
@@ -0,0 +1,35 @@
+package com.example.trainee_test.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.trainee_test.R
+import com.example.trainee_test.databinding.FragmentErrorBinding
+import com.example.trainee_test.databinding.FragmentUsdListBinding
+
+class ErrorFragment : Fragment() {
+
+    private lateinit var binding: FragmentErrorBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentErrorBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnTryAgain.setOnClickListener { it ->
+            findNavController().navigate(R.id.action_errorFragment_to_USDFragment)
+        }
+    }
+}
